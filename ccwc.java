593a01db9ec1d6937ddc7ed150965309d596c500
Parminder Singh <parmindersingh1086@gmail.com>
2024-11-19 02:02:21 -0500

    Refactored code to get ready for next step of project.

--- ccwc.java
+++ ccwc.java
@@ -4,23 +4,26 @@ import java.io.IOException;
 
 public class ccwc {
     public static void main(String[] args){
-        if(args.length != 2 || !args[0].equals("-c")){
-            System.out.println("Error incorrect usage" + args[0]);
-            return;
-        }
 
         String filename = args[1];
 
         try(FileInputStream finput = new FileInputStream(filename)){
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            int byteCount = 0;
 
-            while((bytesRead = finput.read(buffer)) != -1){
-                byteCount += bytesRead;
+            if(args[0].equals("-c")){
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                int byteCount = 0;
+
+                while((bytesRead = finput.read(buffer)) != -1){
+                    byteCount += bytesRead;
+                }
+
+                System.out.println(byteCount);
             }
 
-            System.out.println(byteCount);
+            else if(args[0].equals("-l")){
+                int lineCount = 0;
+            }
         } catch(IOException e){
             System.err.println("Error reading the file: " + e.getMessage());
         }
