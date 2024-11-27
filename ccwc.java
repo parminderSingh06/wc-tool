015c564df3b0986c24c40e8144cc61faabca4b71
Parminder Singh <parmindersingh1086@gmail.com>
2024-11-27 15:06:42 -0500

    Stopped tracking certain files and refactored code

--- ccwc.java
+++ ccwc.java
@@ -1,32 +1,42 @@
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 
 
 public class ccwc {
     public static void main(String[] args){
 
-        String filename = args[1];
+        if(args.length != 2 || !args[0].equals("-c")){
+            System.out.println("Error incorrect usage " + args[0]);
+            return;
+        }
+        
+        argsDesignater(args[0], args[1]);
 
-        try(FileInputStream finput = new FileInputStream(filename)){
+    }
 
-            if(args[0].equals("-c")){
-                byte[] buffer = new byte[1024];
-                int bytesRead;
-                int byteCount = 0;
+    public static void argsDesignater(String argsType, String fileName){
+        if(argsType.equals("-c")) getBytes(fileName);
+    }
+    
+    public static void getBytes(String fileName){
 
-                while((bytesRead = finput.read(buffer)) != -1){
-                    byteCount += bytesRead;
-                }
+        try(FileInputStream finput = new FileInputStream(fileName)){
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            int byteCount = 0;
 
-                System.out.println(byteCount);
+            while((bytesRead = finput.read(buffer)) != -1){
+                byteCount += bytesRead;
             }
 
-            else if(args[0].equals("-l")){
-                int lineCount = 0;
-            }
+            System.out.println(byteCount);
         } catch(IOException e){
             System.err.println("Error reading the file: " + e.getMessage());
         }
+
     }
+
     
 }
